bd277377f12655448c475097d8489ab71a04c89c
taqiismail10 <taqiismail10@gmail.com>
2025-04-24 12:27:56 +0600

    Scaffold backend API with basic user route

---
+++ server/controllers/userController.js
@@ -0,0 +1,15 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: "Server Error" });
+  }
+};
+
+module.exports = {
+  getAllUsers,
+};
